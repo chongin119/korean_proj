fc2540d2cf00f939859125d785804b9ed2024510
chongin119 <macaotom@gmail.com>
2022-03-08 01:25:27 +0800

    update sql and func

--- backend/db/db_config.py
+++ backend/db/db_config.py
@@ -1,7 +1,7 @@
 Config = {
     "path" : {
-        "book1" : "韩国语1第一课.txt",
-        "book2" : "韩国语2第一课.txt",
-        "book3" : "韩国语4第一课.txt"
+        "book1" : "./source/韩国语1第一课.txt",
+        "book2" : "./source/韩国语2第一课.txt",
+        "book3" : "./source/韩国语4第一课.txt"
     }
 }
--- backend/db/db_control.py
+++ backend/db/db_control.py
@@ -7,7 +7,17 @@ class DB:
 
 
     def __del__(self):
+        self.db.close()
+
+    def insert(self):
         pass
 
 if __name__ == "__main__":
     db = DB("korean.db")
+
+    test = []
+
+    for i,pth in enumerate(Config["path"].values()):
+        with open(pth,'r',encoding="utf-16") as f:
+            test = f.read()
+            print(test)
